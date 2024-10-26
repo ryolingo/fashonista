215d9a4795b473e8f82d67fa182ea1041952edd0
ryolingo <ryomatsuya04@gmail.com>
2024-10-27 04:35:50 +0900

    IconをLogoutボタンとして作成

--- src/components/Logoutbutton.tsx
+++ src/components/Logoutbutton.tsx
@@ -1,14 +1,18 @@
 'use client';
 
 import { signOut } from 'next-auth/react';
+import Image from 'next/image';
 
-export default function LogoutButton() {
+export default function LogoutButton({ iconUrl }: { iconUrl: string }) {
    return (
-      <button
-         onClick={() => signOut({ callbackUrl: '/' })}
-         className='border rounded-md bg-blue-200 hover:bg-blue-300'
-      >
-         サインアウト
+      <button onClick={() => signOut({ callbackUrl: '/' })}>
+         <Image
+            src={iconUrl} // アイコン画像のURL
+            alt='Icon Image' // 画像の代替テキスト
+            width={50} // 適切な幅
+            height={50} // 適切な高さ
+            className='rounded-full mr-5' // クラス名を正しく修正
+         />
       </button>
    );
 }
