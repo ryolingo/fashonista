d69628cc3eead2f424b2caa36a60a856277f6f42
kondai24 <DaisukeKondo1215@outlook.jp>
2024-10-26 16:52:02 +0900

    🎨 nextAuthの設計

---
+++ src/lib/auth-options.ts
@@ -0,0 +1,37 @@
+import { FirestoreAdapter } from '@auth/firebase-adapter';
+import { firestore } from './firebase';
+import Google from 'next-auth/providers/google';
+import { NextAuthOptions } from 'next-auth';
+
+export const authOptions: NextAuthOptions = {
+   providers: [
+      Google({
+         clientId: process.env.GOOGLE_CLIENT_ID!,
+         clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      }),
+   ],
+   adapter: FirestoreAdapter(firestore),
+   pages: {
+      signIn: '/login',
+   },
+   callbacks: {
+      async session({ session, token }) {
+         if (token) {
+            session.user.userId = token.userId;
+            session.user.name = token.name;
+            session.user.email = token.email;
+            session.user.image = token.picture;
+         }
+         return session;
+      },
+      async jwt({ token, user }) {
+         if (user) {
+            return { ...token, userId: user.id };
+         }
+         return token;
+      },
+   },
+   session: {
+      strategy: 'jwt',
+   },
+};
