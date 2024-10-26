779b2fa4eb448ab714ceb07d8fbbccb05765cd0a
ryolingo <ryomatsuya04@gmail.com>
2024-10-27 04:34:19 +0900

    Logout時のMypage

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,8 +1,9 @@
 import LogoutButton from '@/components/Logoutbutton';
 import { getCurrentUser } from '@/lib/session';
-import { signOut } from 'next-auth/react';
-import Image from 'next/image';
 import { redirect } from 'next/navigation';
+import MainDisplay from '../components/Main-display';
+import Header from '@/components/header';
+import DisplayAvator from '@/components/display-avator';
 
 export default async function mypage() {
    const currentUser = await getCurrentUser();
@@ -10,21 +11,15 @@ export default async function mypage() {
       redirect('/login');
    }
    return (
-      <div className='mt-20 flex justify-center'>
-         <div className='grid gap-5 text-center'>
-            <h1 className='text-2xl font-extrabold'>This is My Page</h1>
-            <p className='text-xl'>hello {currentUser.name}</p>
-            {currentUser.image && (
-               <Image
-                  src={currentUser.image}
-                  width={100}
-                  height={100}
-                  alt='image'
-                  className='rounded-full ml-9'
-               />
-            )}
-            <LogoutButton />
-         </div>
-      </div>
+      <>
+         <Header>
+            <DisplayAvator
+               name={currentUser.name!}
+               email={currentUser.email!}
+               avatarUrl={currentUser.image!}
+            />
+         </Header>
+         <MainDisplay />
+      </>
    );
 }
