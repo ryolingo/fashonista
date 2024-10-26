a7ea9f043dc17d5e56d2d531f6a08bc2f8aaf874
Tomoya <ichikawatomoya0705@gmail.com>
2024-10-26 16:13:16 +0900

    修正_firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,10 +1,19 @@
-import { initFirestore } from '@auth/firebase-adapter';
-import { cert } from 'firebase-admin/app';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
-export const firestore = initFirestore({
-   credential: cert({
-      projectId: process.env.FIREBASE_PROJECT_ID,
-      clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-      privateKey: process.env.FIREBASE_PRIVATE_KEY,
-   }),
-});
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.FIREBASE_APIKEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STRAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MASSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// Initialize Cloud Firestore and get a reference to the service
+const db = getFirestore(app);
+
+export default db;
