8aabc93dffa644d3b66f57991664625679b5a531
kondai24 <DaisukeKondo1215@outlook.jp>
2024-10-26 17:47:11 +0900

    Save local changes to page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,16 @@
+import Link from 'next/link';
+
 export default function Home() {
-  return <div> </div>;
+   return (
+      <div className=' mt-20 flex justify-center'>
+         <div className=''>
+            <div className='grid gap-6 text-center'>
+               <h1 className='text-2xl font-extrabold'>Home Page</h1>
+               <Link href={'/login'} className='border rounded-md bg-blue-200 hover:bg-blue-300'>
+                  ログイン
+               </Link>
+            </div>
+         </div>
+      </div>
+   );
 }
