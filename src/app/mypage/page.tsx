a5e703f558be2741ab542c51b31394118c61d527
kondai24 <DaisukeKondo1215@outlook.jp>
2024-10-28 20:15:26 +0900

    🎨 clothesの情報を付与

--- src/app/mypage/page.tsx
+++ src/app/mypage/page.tsx
@@ -1,15 +1,18 @@
 import LogoutButton from '@/components/Logoutbutton';
 import { getCurrentUser } from '@/lib/session';
 import { redirect } from 'next/navigation';
+import { ClothingList } from '@/components/ClothingCardList';
 import MainDisplay from '@/components/Main-display';
 import Header from '@/components/header';
 import DisplayAvator from '@/components/display-avator';
+import { getClothingItems } from '@/lib/firestore';
 
 export default async function mypage() {
    const currentUser = await getCurrentUser();
    if (!currentUser) {
       redirect('/login');
    }
+   const clothingItems = await getClothingItems();
    return (
       <>
          <Header>
@@ -20,6 +23,7 @@ export default async function mypage() {
             />
          </Header>
          <MainDisplay />
+         <ClothingList items={clothingItems} />
       </>
    );
 }
