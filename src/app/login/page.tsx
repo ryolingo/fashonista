94698ed257d62511c21add1f7321e10bc5bc6ba2
ryolingo <ryomatsuya04@gmail.com>
2024-11-18 15:08:41 +0900

    不要なimportの削除

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,10 +1,6 @@
 'use client';
-
 import Header from '@/components/header';
 import LoginForm from '@/components/login-form';
-import { buttonVariants } from '@/components/ui/button';
-import { cn } from '@/lib/utils';
-import { signIn } from 'next-auth/react';
 
 export default function login() {
    return (
